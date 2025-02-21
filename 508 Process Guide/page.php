5db5258bb0d9ccc97967df44cd41c3162002de36
mlundy2 <77565109+mlundy2@users.noreply.github.com>
2025-02-21 01:15:18 -0500

    Set dynamic intro text
    Set up the intro text per page dynamically

--- 508 Process Guide/page.php
+++ 508 Process Guide/page.php
@@ -10,13 +10,23 @@
 </div>
 
 
-
-<div class="guidebook-section-intro">
-    <h1 class="guidebook-section-intro-header" id="section-name"></h1>
-    <p class="guidebook-section-intro-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, facere. Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates labore nisi voluptate repudiandae molestiae ad!</p>
+<div class="sub-page-intro">
+    <h1 class="sub-page-intro-header" id="section-name"></h1>
+    <p class="sub-page-intro-text" id="sub-page-intro-text"></p>
 </div>
 
 <script>
+    let fetchData1 = async (url, func) => {
+        let res = await fetch(url);
+        let data = await res.json();
+
+        return func(data);
+    }
+
+    let getURL1 = (file) => {
+        return `data/${file}.json`;
+    }
+    
     const imgSRC = sessionStorage.getItem('imgSRC');
     const pageName = sessionStorage.getItem('category');
 
@@ -28,6 +38,22 @@
 
     let pageTitle = document.getElementById("page-title");
     pageTitle.innerHTML = pageName;
+
+    function setText(data) {
+        let pageIntroText = document.getElementById("sub-page-intro-text");
+        let output = "";
+
+        // Loop through the sorted data and create the HTML output
+        for (let i = 0; i < data.general.length; i++) {
+            for (let j = 0; j < data.general[i].categories.length; j++) {
+                if(data.general[i].categories[j].category == `${pageName}`) {
+                    pageIntroText.innerHTML = data.general[i].categories[j].intro;
+                }
+            }
+        }
+    }
+
+    fetchData1(getURL1("topics"), setText);
 </script>
 
 <?php include "inc/footer.php" ?>
